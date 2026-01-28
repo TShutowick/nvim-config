e4e004b9541cc4d4db79d028921be4620591b404
Tyler Shutowick <tylershutowick@gmail.com>
2026-01-28 09:30:17 -0800

    gitsigns, lua formatting, gruvbox

---
+++ ftplugin/lua.lua
@@ -0,0 +1,5 @@
+-- Settings specifically for Lua files
+vim.opt_local.expandtab = true   -- Convert tabs to spaces
+vim.opt_local.shiftwidth = 2     -- Indent by 2 spaces
+vim.opt_local.tabstop = 2        -- Visual width of a tab
+vim.opt_local.softtabstop = 2    -- Tab key behaves like 2 spaces
